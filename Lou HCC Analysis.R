1e57628f360b4569ca49836be7261a5cc05b83f7
kglass11 <katherine.glass@lshtm.ac.uk>
2018-08-16 11:49:57 +0100

    Beginning new script for analysis of Lou Pk HCC Q-dot Isotypes Study
    Keep in mind most things will be done outside of R.

---
+++ Lou HCC Analysis.R
@@ -0,0 +1,22 @@
+#Lou Pk HCC Q-dot Study Analysis 
+#August 16, 2018
+
+###Clear the environnment - OR go to Session > Clear Workspace
+rm(list=ls())
+
+#set working directory
+setwd("/Users/Katie/Desktop/R files from work/Lou HCC/IgG")
+getwd()
+
+#Import data from IgG, IgA, or IgM
+load(file = "Pk_HCC_analysis_IgG.RData")
+
+#Standard Plots with all standards
+
+#Seropositivity Cutoffs
+
+#Calculations for total number of people reactive to each Pk antigen 
+
+#Calculations and plot for antigen breadth 
+
+
