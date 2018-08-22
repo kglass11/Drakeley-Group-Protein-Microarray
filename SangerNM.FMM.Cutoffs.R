d1c9b5f7450818450a8b6b6b9b739564dad716b7
kglass11 <katherine.glass@lshtm.ac.uk>
2018-08-22 21:05:34 +0100

    new file for FMMs for Sanger NM antigens for seropositivity cutoffs
    this is based on Yuyen's file Mixture_Model2c.R at this point

---
+++ SangerNM.FMM.Cutoffs.R
@@ -0,0 +1,19 @@
+#Sanger NM FMM Seropositivity Cutoffs
+
+rm(list=ls())
+
+setwd("/Users/Katie/Desktop/R files from work/100817 Sanger/Sanger Data Processing KG")
+
+load(file = "Sanger.2.Update.RData")
+
+require("gtools")
+# 
+# library(limma)
+# library(contrast)
+# library(beeswarm)
+# library(mixtools)
+# library(gplots)
+# library(ggplot2)
+# library(gcookbook)
+# library(dplyr)
+
