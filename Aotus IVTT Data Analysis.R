b403d0e33aff63bede2372a4f08bf21f12192279
Katherine Glass <katherine.glass@lshtm.ac.uk>
2018-09-27 17:24:47 +0100

    working on seropositivity cutoffs - still in middle

--- Aotus IVTT Data Analysis.R
+++ Aotus IVTT Data Analysis.R
@@ -45,6 +45,61 @@ sampleinfo$DAY <- as.character(sampleinfo$DAY)
 sampleinfo$slide_no <- as.character(sampleinfo$slide_no)
 sampleinfo$block_rep_1 <- as.character(sampleinfo$block_rep_1)
 
+#maybe should look at the data before determining how to do the cutoffs, 
+#whether to do them for each sample or each antigen
+
+# seropositivity cutoffs for each antigen based on mean + 3SD of reactivity of uninfected monkeys (time zero)
+#use data where the negatives have been set to 0 or not? look at data then decide 
+
+#norm_sub4.df has data with control targets removed, no control samples removed;
+#GST subtracted, negative values set to 0 data 
+
+#prepare overall data frame with all data and sample metadata for filtering 
+normsub4T <- t(norm_sub4.df)
+
+info_data <- merge(sampleinfo, normsub4T, by.y = "row.names", by.x = "sample_id", sort = FALSE)
+
+#isolate data for time 0 (uninfected monkeys)
+uninfected <- filter(info_data, DAY_POST == "-1")
+rownames(uninfected) <- uninfected$sample_id_unique
+
+uninf.data <- uninfected[,(ncol(sampleinfo)+1):ncol(uninfected)]
+
+#it won't work to define a cutoff based on this data because there are too many zeros, won't get a good SD. 
+#if we had more controls, i.e. controls over all time points, then we could maybe do this.
+
+#going back to the buffer spots method --> in this case will be no DNA control spots, 
+#same used for normalizing for Lou's and other studies
+
+#Seropositivity above a threshold of mean of sample specific buffer spots + 3SD. 
+#targets_buffer was changed to No DNA spots, so everything with buffer actually refers to that
+sample_cutoff <- cor2_buffer_sample_mean + 3*cor2_buffer_sample_sd
+log_sample_cutoff <- log2(sample_cutoff)
+norm_sample_cutoff <- log_sample_cutoff - log_buffer_sample_mean
+
+#Tailor the norm_sample_cutoff to remove excluded samples and control samples
+buffer_cutoff.matrix <- as.matrix(norm_sample_cutoff)
+rownames(buffer_cutoff.matrix, colnames(norm4.matrix))
+sub_cutoff <- buffer_cutoff.matrix[(!rownames(buffer_cutoff.matrix) %in% samples_exclude),]
+
+#Plot the sample cutoffs for samples included in analysis
+png(filename = paste0(study, "_Buffer_Cutoffs.tif"), width = 5, height = 4, units = "in", res = 1200)
+par(mfrow=c(1,1), oma=c(3,1,1,1),mar=c(4.1,4.1,3.1,2.1))
+plot(sub_cutoff, pch='*', col = "blue", ylim=c(0,max(sub_cutoff)*1.25),
+     ylab="Seropositivity Cutoff", xlab="Sample (Array)")
+
+graphics.off()
+
+#### pick up from here tomorrow, the below needs workd
+
+#At this point, Remove control samples for further analysis
+norm_sub6.df <- norm_sub5.df[,colnames(norm_sub5.df) %in% samples_test]
+
+#Then can apply the norm_sample_cutoff all antigens
+seropos.matrix <- t(apply(norm_sub4.df, 1, function(x) ((x > sub_cutoff)+0)))
+
+
+
 
 
 
