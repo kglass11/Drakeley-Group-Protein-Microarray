719314ab90025d84c8deb6aa6f200268eba7aae3
kglass11 <katherine.glass@lshtm.ac.uk>
2019-01-15 16:22:11 -0700

    prepping analysis script, update to duplicate samples CV

--- KenebaAnalysisBigStudy.R
+++ KenebaAnalysisBigStudy.R
@@ -1,27 +1,48 @@
-### Keneba Study Big Study Analysis 
+### Keneba Big Study Analysis 
+#Katie Glass
+#updated: Jan 15 2019
+
+setwd("/Users/Katie/Desktop/R files from work/Keneba main results/Keneba Analysis")
+
+#load IgG or IgM 
+load("Keneba_IgG_v3_AfterProcessing.RData")
+
+#load packages
+library(contrast)
+library(beeswarm)
+library(mixtools)
+library(gplots)
+library(ggplot2)
+library(gcookbook)
+library(dplyr)
+library(reshape2)
+library(outliers)
+library(corrgram)
+library(corrplot)
 
 
-## currently continuing on from partway through the processing script - IgG
 
-### duplicated samples QC
-#there was a replicated sample ID measured 9 times to get the interarray repeatability data 
+#####################################
+############DATA ANALYSIS############
+#####################################
 
-#isolate replicated data
+####### duplicated samples QC
+  #there was a replicated sample ID measured 9 times to get the interarray repeatability data 
 
-#post normalization and GST subtraction and replicate averaging, nothing set to 0
-sample_rep <- grep("NKC821Y", colnames(quadrulemat))
-NKC821Ydata <- quadrulemat[,sample_rep]
+  #isolate replicated data
+  #post normalization and GST subtraction and replicate averaging, nothing set to 0
+  sample_rep <- grep("NKC821Y", colnames(quadrulemat))
+  NKC821Ydata <- quadrulemat[,sample_rep]
 
-CV <- apply(NKC821Ydata, 1, sd, na.rm = TRUE) / rowMeans(NKC821Ydata, na.rm = TRUE)
-avgCV <- mean(CV, na.rm = TRUE ) * 100 
+  CV <- apply(NKC821Ydata, 1, sd, na.rm = TRUE) / rowMeans(NKC821Ydata, na.rm = TRUE)
+  avgCV <- mean(CV, na.rm = TRUE ) * 100 
 
-# but it doesn't make sense to get CV of negative numbers or spots where everything is close to 0
-
-#so let's only use the CV where the mean is > = to 1 (double buffer values)
-#this is getting 60 spots total out of 144
-NKC821Ygr1 <- NKC821Ydata[which(rowMeans(NKC821Ydata, na.rm = TRUE) > 1),]
-CV.3 <- apply(NKC821Ygr1, 1, sd, na.rm = TRUE) / rowMeans(NKC821Ygr1, na.rm = TRUE)
-avgCV.3 <- mean(CV.3, na.rm = TRUE ) * 100 #12.15% CV IgG, 11.38% for IgM
+  # but it doesn't make sense to get CV of negative numbers or spots where everything is close to 0
+  #so let's only use the CV where the mean is > = to 1 (double buffer values)
+  #this is getting 60 spots total out of 144
+  NKC821Ygr1 <- NKC821Ydata[which(rowMeans(NKC821Ydata, na.rm = TRUE) > 1),]
+  CV.3 <- apply(NKC821Ygr1, 1, sd, na.rm = TRUE) / rowMeans(NKC821Ygr1, na.rm = TRUE)
+  avgCV.3 <- mean(CV.3, na.rm = TRUE ) * 100 #12.155% CV IgG, 11.38% for IgM
 
 ####### IgG vs IgM standard reactivity 
 
@@ -42,4 +63,7 @@ if(iso == "IgM"){
   stdratioFc <- IgMfc_high_mean/IgG_high_mean
   eratioFc <- 2^stdratioFc #110338167
 }
+
+######Histograms of all the data for each antigen
+
 
