5a08b6e0b74bb2ebe845d18b2815a32c5623db67
kglass11 <katherine.glass@lshtm.ac.uk>
2019-04-17 13:51:45 -0700

    isolating relevant data - Pf antigens only, separate for test and controls

--- Sanger Malaria ROC.R
+++ Sanger Malaria ROC.R
@@ -28,3 +28,49 @@ library(corrplot)
 library(ggbeeswarm)
 
 load(file="Sanger.2.Update.RData")
+
+###### Isolate data for malarial antigens and test samples only (no controls) or test samples and controls
+###negative values are included here
+###this first section is repeated from FMM script (Sanger Analysis Non-Malarial Antigens)
+
+#Remove control protein targets
+#Remove samples that should be excluded and isolate test samples - leaves 1325 samples
+#This means that the two neg pools and 6 malaria positive controls are not affecting the FMMs
+subdata <- norm3.matrix[-rmsamp_all,!(colnames(norm3.matrix) %in% samples_exclude)]
+testdata <- subdata[,(colnames(subdata) %in% samples_test)]
+
+#Replace current target names with original target names now that control targets are removed
+testdata.df <- merge(testdata, annotation_targets.df, by ="row.names", sort = FALSE)
+testdata.df <- tibble::column_to_rownames(testdata.df, var="Row.names")
+row.names(testdata.df) <- testdata.df$Name
+Ftestdata <- testdata.df[,1:ncol(testdata)]
+
+#do the same thing for the matrix that has the controls - need this later
+#Replace current target names with original target names now that control targets are removed
+subdata.df <- merge(subdata, annotation_targets.df, by ="row.names", sort = FALSE)
+subdata.df <- tibble::column_to_rownames(subdata.df, var="Row.names")
+row.names(subdata.df) <- subdata.df$Name
+Fsubdata <- subdata.df[,1:ncol(subdata)]
+
+#Merge with target metadata to filter based on expression tag etc.
+target.df <- merge(target_meta.df, Ftestdata, by.x = "Name", by.y ="row.names", all.y = TRUE, sort = FALSE)
+targetsub.df <- merge(target_meta.df, Fsubdata, by.x = "Name", by.y ="row.names", all.y = TRUE, sort = FALSE)
+
+#isolate data for malarial antigens - falciparum only!!! 
+#Note, different dilutions are still included.
+NMdatameta <- filter(target.df, Plasmodium == "Pf")
+NMdata <- NMdatameta[,(ncol(target_meta.df)+1):ncol(NMdatameta)]
+rownames(NMdata) <- NMdatameta$Name
+
+min(NMdata) #-8.92014
+max(NMdata) #9.999541
+
+#repeat for subdata (test and control samples)
+NMallsampmeta <- filter(targetsub.df, Plasmodium == "Pf")
+NMallsampdata <- NMallsampmeta[,(ncol(target_meta.df)+1):ncol(NMallsampmeta)]
+rownames(NMallsampdata) <- NMallsampmeta$Name
+
+#Transpose data so antigens are columns - it's now a matrix
+tNMdata <- t(NMdata)
+
+tNMallsampdata <- t(NMallsampdata)
